2fceec91ab7d56cc1ba911a58f6167b6ab2a093f
Harro Lissenberg <harro.lissenberg@gmail.com>
2013-01-21 20:54:00 +0100

    Updated twitter polling

---
+++ src/main/java/com/lissenberg/tinker/Servo.java
@@ -0,0 +1,48 @@
+package com.lissenberg.tinker;
+
+
+import com.tinkerforge.BrickServo;
+import com.tinkerforge.IPConnection;
+
+public class Servo {
+
+    private static final Short S1 = 2;
+    private static final Short S2 = 5;
+
+    public static void main(String[] args) throws Exception {
+
+        IPConnection ip = new IPConnection("localhost", 4223);
+
+        BrickServo servo = new BrickServo("94yAGXiUGQz");
+        ip.addDevice(servo);
+
+        servo.setOutputVoltage(4800);
+
+        servo.setDegree(S1, Short.MIN_VALUE, Short.MAX_VALUE);
+        servo.setPulseWidth(S1, 1000, 2000);
+        servo.setAcceleration(S1, 0xFFFF);
+        servo.setVelocity(S1, 0xFFFF); // Full speed
+
+        servo.setDegree(S2, Short.MIN_VALUE, Short.MAX_VALUE);
+        servo.setPulseWidth(S2, 1000, 2000);
+        servo.setAcceleration(S2, 0xEEEE);
+        servo.setVelocity(S2, 0xFFFF); // Full speed
+
+        servo.enable(S1);
+        servo.enable(S2);
+        for (int i = 0; i < 10; i++) {
+            servo.setPosition(S1, Short.MIN_VALUE);
+            servo.setPosition(S2, Short.MAX_VALUE);
+            Thread.sleep(2000l);
+            servo.setPosition(S1, Short.MAX_VALUE);
+            servo.setPosition(S2, Short.MIN_VALUE);
+            Thread.sleep(2000l);
+            servo.setPosition(S1, Short.MIN_VALUE);
+            servo.setPosition(S2, Short.MAX_VALUE);
+
+        }
+
+        ip.destroy();
+
+    }
+}
--- src/main/java/com/lissenberg/tinker/Tweet.java
+++ src/main/java/com/lissenberg/tinker/Tweet.java
@@ -9,44 +9,80 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Searches for tweets and displays result on LCD
  */
 public class Tweet {
     public static final String KEYWORD = "#tinkerforge";
+    public static final int NR = 5;
+    public static final int SECS = 5;
+    public static BrickletLCD20x4 lcd;
+
 
     public static void main(String[] args) throws Exception {
         IPConnection ip = new IPConnection("localhost", 4223);
-        BrickletLCD20x4 lcd = new BrickletLCD20x4("bfL");
+        lcd = new BrickletLCD20x4("bfL");
         ip.addDevice(lcd);
 
-        TweetData tweet = findTweet(KEYWORD);
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleWithFixedDelay(new Poller(), 0, NR * SECS, TimeUnit.SECONDS);
 
         lcd.backlightOn();
         lcd.clearDisplay();
-        if (tweet != null) {
-            lcd.writeLine((short) 0, (short) 0, tweet.from);
-            String[] strings = threeLines(tweet.text);
-            lcd.writeLine((short) 1, (short) 0, strings[0]);
-            lcd.writeLine((short) 2, (short) 0, strings[1]);
-            lcd.writeLine((short) 3, (short) 0, strings[2]);
-        } else {
-            lcd.writeLine((short) 0, (short) 0, "No tweets for:");
-            lcd.writeLine((short) 1, (short) 0, KEYWORD);
-        }
 
         System.in.read();
+
+        executor.shutdown();
+        executor.awaitTermination(NR * SECS, TimeUnit.SECONDS);
 
         lcd.backlightOff();
         ip.destroy();
     }
 
+    private static void updateLCD(TweetData tweet) {
+        if (tweet == null) {
+            lcd.writeLine((short) 0, (short) 0, "No tweets for:");
+            lcd.writeLine((short) 1, (short) 0, KEYWORD);
+        } else {
+            lcd.writeLine((short) 0, (short) 0, tweet.from);
+            String[] strings = threeLines(tweet.text);
+            lcd.writeLine((short) 1, (short) 0, strings[0]);
+            lcd.writeLine((short) 2, (short) 0, strings[1]);
+            lcd.writeLine((short) 3, (short) 0, strings[2]);
+        }
+    }
 
-    private static TweetData findTweet(String query) throws Exception {
-        String q = String.format("q=%s&rpp=1&include_entities=false&result=recent",
+    public static class Poller implements Runnable {
+        @Override
+        public void run() {
+            try {
+                List<TweetData> tweets = findTweets(KEYWORD);
+                if (tweets.isEmpty()) {
+                    updateLCD(null);
+                } else {
+                    for (TweetData tweet : tweets) {
+                        updateLCD(tweet);
+                        Thread.sleep(SECS * 1000);
+                    }
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+    private static List<TweetData> findTweets(String query) throws Exception {
+        List<TweetData> tweets = new ArrayList<>();
+        String q = String.format("q=%s&rpp=" + NR + "&include_entities=false&result=recent",
                 URLEncoder.encode(query, "UTF-8"));
         URL search = new URL("http://search.twitter.com/search.json?" + q);
         HttpURLConnection con = (HttpURLConnection) search.openConnection();
@@ -61,13 +97,14 @@ public class Tweet {
         List<Map<String, Object>> results = (List<Map<String, Object>>) map.get("results");
         TweetData r = null;
         if (results.size() > 0) {
-            r = new TweetData();
             for (Map<String, Object> result : results) {
+                r = new TweetData();
                 r.from = "@" + result.get("from_user").toString();
                 r.text = result.get("text").toString();
+                tweets.add(r);
             }
         }
-        return r;
+        return tweets;
     }
 
     static class TweetData {
